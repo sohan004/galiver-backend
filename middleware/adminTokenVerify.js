2938c60d1bc07606e01d87fb471bdafc98dcce06
sohan004 <aryansohan02@gmail.com>
2024-10-12 13:40:01 +0600

    Changes committed on 2024-10-12 13:40:01

--- middleware/adminTokenVerify.js
+++ middleware/adminTokenVerify.js
@@ -8,7 +8,7 @@ const adminTokenVerify = async (req, res, next) => {
             return res.status(400).json({ message: 'Unauthorize Request' });
         }
         const decode = await jwt.verify(token, process.env.JWT_SECRET);
-        const findUser = await User.findById(decode.userID).select('role')
+        const findUser = await User.findById(decode.id).select('role')
         if (!findUser) {
             return res.status(400).json({ message: 'User not found' });
         }
